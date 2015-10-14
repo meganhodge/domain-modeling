7be006cdbd9ddaab16c7050225648d01fb5faa83
Megan Hodge <mhodge16@uw.edu>
2015-10-13 18:42:47 -0700

    first commit

---
+++ domain-modeling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  domain-modeling
+//
+//  Created by iGuest on 10/13/15.
+//  Copyright (c) 2015 Megan Hodge. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
